49609118db1b2c31ed63e92e53a4c550b5a9bf1b
gurdeepsainipb65 <gurdeepsainig2001@gmail.com>
2025-02-17 16:42:35 +0530

    hghg

--- passportConfig.js
+++ passportConfig.js
@@ -2,14 +2,14 @@ const dotenv = require("dotenv");
 dotenv.config();
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
-const { baseURL } = require(".");
+const { baseURL } = require("./index");
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${baseURL}/auth/google/callback`,
+      callbackURL: `https://blogswebsitebackend.onrender.com/auth/google/callback`,
     },
     (accessToken, refreshToken, profile, done) => {
       done(null, profile); // Send user profile to session
