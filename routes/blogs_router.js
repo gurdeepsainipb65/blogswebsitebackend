eeb5c633e189174bf08d108873c4b2d399801b38
gurdeepsainipb65 <gurdeepsainig2001@gmail.com>
2025-02-22 17:27:39 +0530

    update

--- routes/blogs_router.js
+++ routes/blogs_router.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const Blogs_router = express.Router();
 const Blogs = require("../models/blogs_models");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+const blogs = require("../models/blogs_models");
+
+function verifyToken(req, resp, next) {
+  const token = req.headers.authorization;
+  if (!token) {
+    return resp.json({ success: false, message: "Access Denied" });
+  }
+  const data = jwt.verify(token, "mysecret12345");
+  req.user = data;
+  next();
+};
 
 Blogs_router.get("/", async (req, resp) => {
   const blogs = await Blogs.find().populate("user");
@@ -10,18 +21,21 @@ Blogs_router.get("/", async (req, resp) => {
 
 Blogs_router.post("/add", verifyToken, async (req, resp) => {
   const { name, image, categary, description } = req.body;
-  await Blogs.create({ name, image, categary, description, user: req.user.userId });
+  await Blogs.create({
+    name,
+    image,
+    categary,
+    description,
+    user: req.user.userId,
+  });
   resp.json({ success: "blogs created" });
 });
 
-function verifyToken(req, resp, next){
-  const token = req.headers.authorization
-  if (!token) {
-    return resp.json({success: false, message: "Access Denied"})
-  }
-  const data = jwt.verify(token, "mysecret12345")
-  req.user = data
-  next()
-}
+Blogs_router.get("/userblog", verifyToken, async (req, resp) => {
+  const userId = req.user.userId;
+  const userblog = await Blogs.find({user:userId})
+  resp.json({ blogs: userblog });
+}),
+ 
 
 module.exports = Blogs_router;
